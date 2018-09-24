24337393d5f9ca076542d1f49e928806b68b5c4e
Alessandro Astone <ales.astone@gmail.com>
2018-09-25 00:53:15 +0200

    [multiplayer] initial structure

---
+++ super-cool-game/Assets/Scripts/Multiplayer/Matchmaker.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using NetCode;
+
+public class Matchmaker : MonoBehaviour {
+    TcpClient client;
+
+    // Use this for initialization
+    async void Start () {
+        Debug.Log("Ask for lobby");
+        int port = await GetLobby();
+        Debug.Log("Got lobby on port " + port);
+        Join(port);
+	}
+
+    private async Task<int> GetLobby() {
+        client = new TcpClient();
+        client.Connect(NetHelpers.SERVER_ADDR, NetHelpers.MATCHMAKER_PORT);
+        var stream = client.GetStream();
+
+        byte[] message = NetHelpers.BufferOp(OpCode.Queue);
+        await stream.WriteAsync(message, 0, NetHelpers.MESSAGE_SIZE);
+
+        byte[] buffer = new byte[NetHelpers.MESSAGE_SIZE];
+        await stream.ReadAsync(buffer, 0, NetHelpers.MESSAGE_SIZE);
+
+        byte op = buffer[0];
+        if(op == (byte)OpCode.FoundMatch) {
+            UInt16 port = NetHelpers.ReadUInt16(buffer, 1);
+            return port;
+        }
+        return -1;
+    }
+
+    private void Join(int port) {
+        UdpClient socket = new UdpClient();
+        socket.Connect(NetHelpers.SERVER_ADDR, port);
+
+        byte[] buffer = NetHelpers.BufferOp(OpCode.Spawn);
+        socket.Send(buffer, NetHelpers.MESSAGE_SIZE);
+    }
+}
---
+++ super-cool-game/Assets/Scripts/Multiplayer/NetCode.cs
@@ -0,0 +1,34 @@
+﻿namespace NetCode {
+    using System;
+
+    public enum OpCode {
+        Error,
+        Queue,
+        FoundMatch,
+        Spawn,
+        Move
+    }
+
+    public static class NetHelpers {
+        public const string SERVER_ADDR = "localhost";
+        public const int MATCHMAKER_PORT = 50999;
+        public const int MESSAGE_SIZE = 4;
+
+        public static UInt16 ReadUInt16(byte[] buffer, int idx) {
+            return (UInt16)((buffer[idx] << 8) | buffer[idx+1]);
+        }
+
+        public static void WriteUInt16(UInt16 n, ref byte[] buffer, int idx) {
+            if (idx + sizeof(UInt16) > buffer.Length)
+                throw new IndexOutOfRangeException("Buffer cannot contain 2 bytes from index " + idx);
+            buffer[idx] = (byte) (n >> 8);
+            buffer[idx + 1] = (byte) (n & 0x00FF);
+        }
+
+        public static byte[] BufferOp(OpCode op){
+            byte[] buffer = new byte[MESSAGE_SIZE]; // By default filled with zeroes
+            buffer[0] = (byte) op;
+            return buffer;
+        }
+    }
+}
