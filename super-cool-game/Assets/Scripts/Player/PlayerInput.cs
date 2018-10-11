6c5f2bdcbadc51758001515ff87116d30735d2a2
Andrea Franchini <f.andrea1602@gmail.com>
2018-10-11 19:46:45 +0200

    Added Player UI and two blocks sprites

--- super-cool-game/Assets/Scripts/Player/PlayerInput.cs
+++ super-cool-game/Assets/Scripts/Player/PlayerInput.cs
@@ -6,9 +6,11 @@ public class PlayerInput : MonoBehaviour {
     /* Handles user inputs, to be sent over to PlayerController */
 
     private PlayerController controller;
+    private PlayerUI ui;
 
     void Start() {
         controller = gameObject.GetComponent<PlayerController>();
+        ui = GameObject.Find("/PlayerUICanvas").GetComponent<PlayerUI>();
     }
     
     void Update () {
@@ -18,6 +20,16 @@ public class PlayerInput : MonoBehaviour {
         // Player wants to jumpy
         if (Input.GetButtonDown("Jump")) {
             controller.Jump();
+        }
+
+        // PLAYER UI
+        // Scroll left in block selector menu
+        if (Input.GetButtonDown("ScrollLeft_BM")) {
+            ui.ScrollLeft();
+        }
+        // Scroll right in block selector menu
+        if (Input.GetButtonDown("ScrollRight_BM")) {
+            ui.ScrollRight();
         }
 	}
 }
